ed3ba6a4f3ad92dc8a16829ea1ddcfe321cf3af6
Bryan Culbertson <bryan.culbertson@gmail.com>
2018-09-16 12:34:30 -0700

    modularized, works for multiple pins

--- experiments/buttons/adafruit_mcp3008.py
+++ experiments/buttons/adafruit_mcp3008.py
@@ -3,47 +3,14 @@
 # Written by Limor "Ladyada" Fried for Adafruit Industries, (c) 2015
 # This code is released into the public domain
 
+import click
+from collections import defaultdict
 import time
 import os
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 DEBUG = 1
-
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-        if ((adcnum > 7) or (adcnum < 0)):
-                return -1
-        GPIO.output(cspin, True)
-
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
-
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
-
-        GPIO.output(cspin, True)
-
-        adcout >>= 1       # first bit is 'null' so drop it
-        return adcout
 
 # change these as desired - they're the pins connected from the
 # SPI port on the ADC to the Cobbler
@@ -52,54 +19,98 @@ SPIMISO = 19
 SPIMOSI = 20
 SPICS = 21
 
-# set up the SPI interface pins
-GPIO.setup(SPIMOSI, GPIO.OUT)
-GPIO.setup(SPIMISO, GPIO.IN)
-GPIO.setup(SPICLK, GPIO.OUT)
-GPIO.setup(SPICS, GPIO.OUT)
-
 # 10k trim pot connected to adc #0
-potentiometer_adc = 0;
+read_pins = [0, 2]
+#potentiometer_adc = 0;
 
-last_read = 0       # this keeps track of the last potentiometer value
+# this keeps track of the last value for each pin
+last_read = defaultdict(int)
+pin_changed = defaultdict(lambda: False)
 tolerance = 5       # to keep from being jittery we'll only change
                     # volume when the pot has moved more than 5 'counts'
 
-while True:
-        # we'll assume that the pot didn't move
-        trim_pot_changed = False
+def setup():
+    # set up the SPI interface pins
+    GPIO.setup(SPIMOSI, GPIO.OUT)
+    GPIO.setup(SPIMISO, GPIO.IN)
+    GPIO.setup(SPICLK, GPIO.OUT)
+    GPIO.setup(SPICS, GPIO.OUT)
 
-        # read the analog pin
-        trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        # how much has it changed since the last read?
-        pot_adjust = abs(trim_pot - last_read)
 
-        if DEBUG:
-                print("trim_pot:", trim_pot)
-                #, "pot_adjust:", pot_adjust, "last_read", last_read)
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+    if ((adcnum > 7) or (adcnum < 0)):
+        return -1
+    GPIO.output(cspin, True)
 
-        if ( pot_adjust > tolerance ):
-               trim_pot_changed = True
+    GPIO.output(clockpin, False)  # start clock low
+    GPIO.output(cspin, False)     # bring CS low
 
-        if DEBUG:
-                pass
-                #print("trim_pot_changed", trim_pot_changed)
+    commandout = adcnum
+    commandout |= 0x18  # start bit + single-ended bit
+    commandout <<= 3    # we only need to send 5 bits here
+    for i in range(5):
+        if (commandout & 0x80):
+            GPIO.output(mosipin, True)
+        else:
+            GPIO.output(mosipin, False)
+        commandout <<= 1
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
 
-        if ( trim_pot_changed ):
-                set_volume = trim_pot / 10.24           # convert 10bit adc0 (0-1024) trim pot read into 0-100 volume level
-                set_volume = round(set_volume)          # round out decimal value
-                set_volume = int(set_volume)            # cast volume as integer
+    adcout = 0
+    # read in one empty bit, one null bit and 10 ADC bits
+    for i in range(12):
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
+        adcout <<= 1
+        if (GPIO.input(misopin)):
+            adcout |= 0x1
 
-                #print('Volume = {volume}%' .format(volume = set_volume))
-                set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' .format(volume = set_volume)
-                os.system(set_vol_cmd)  # set volume
+    GPIO.output(cspin, True)
 
-                if DEBUG:
-                        pass
-                        #print("set_volume", set_volume, "tri_pot_changed", set_volume)
+    adcout >>= 1       # first bit is 'null' so drop it
+    return adcout
 
-                # save the potentiometer reading for the next loop
-                last_read = trim_pot
+
+def handle_pin(pin_idx):
+    # read the analog pin
+    trim_pot = readadc(pin_idx, SPICLK, SPIMOSI, SPIMISO, SPICS)
+
+    # how much has it changed since the last read?
+    pot_adjust = abs(trim_pot - last_read[pin_idx])
+
+    #if DEBUG:
+    #    print(f"trim_pot {pin_idx}: {trim_pot}")
+        #, "pot_adjust:", pot_adjust, "last_read", last_read)
+
+    if pot_adjust > tolerance:
+        pin_changed[pin_idx] = True
+
+    if pin_changed[pin_idx]:
+        last_read[pin_idx] = trim_pot
+
+    return trim_pot
+
+
+@click.command()
+@click.option('-p', '--pin', type=int, multiple=True)
+def main(pin):
+    if not pin:
+        read_pins = list(range(8))
+    else:
+        read_pins = pin
+    setup()
+    while True:
+        pin_vals = []
+        for pin_idx in read_pins:
+            pin_vals.append(str(handle_pin(pin_idx)))
+
+        print("Pin Reads:", " ".join(pin_vals))
 
         # hang out and do nothing for a half second
         time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    main()
